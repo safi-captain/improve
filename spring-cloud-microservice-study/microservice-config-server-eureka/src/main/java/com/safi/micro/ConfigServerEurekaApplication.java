5c3f94b8620b317c5047c232fe69e70fa5b564de
safi-captain <xiefeng_626@163.com>
2021-07-18 15:24:25 +0800

    springcloud学习

--- spring-cloud-microservice-study/microservice-config-server-eureka/src/main/java/com/safi/micro/ConfigServerEurekaApplication.java
+++ spring-cloud-microservice-study/microservice-config-server-eureka/src/main/java/com/safi/micro/ConfigServerEurekaApplication.java
@@ -3,13 +3,13 @@ package com.safi.micro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-@EnableFeignClients
-public class MovieFeignApplication {
+public class ConfigServerEurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MovieFeignApplication.class, args);
+        SpringApplication.run(ConfigServerEurekaApplication.class, args);
     }
 }
--- spring-cloud-microservice-study/microservice-consumer-movie-feign/src/main/java/com/safi/micro/controller/FeignController.java
+++ spring-cloud-microservice-study/microservice-consumer-movie-feign/src/main/java/com/safi/micro/controller/FeignController.java
@@ -2,14 +2,19 @@ package com.safi.micro.controller;
 
 import com.safi.micro.entity.User;
 import com.safi.micro.remote.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class FeignController {
+    private static final Logger log = LoggerFactory.getLogger(FeignController.class);
     @Autowired
     private UserFeignClient userFeignClient;
     @Value("${school}")
@@ -17,6 +22,7 @@ public class FeignController {
 
     @GetMapping("feign/{id}")
     public User findByIdFeign(@PathVariable Long id) {
+        log.info(school);
         User user = this.userFeignClient.findByIdFeign(id);
         return user;
     }
